b459046eed33ea1185a6207597a511df7786959d
Alina <kolmykova93@gmail.com>
2020-05-04 21:58:19 +0200

    init commit

---
+++ Rektanglar och Ellipser/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rektanglar_och_Ellipser
+{
+    internal class Program
+    {
+        public enum ShapeType
+        {
+            Rectangle,
+            Ellipse
+        }
+
+        public static Shape CreateShape(ShapeType shape)
+        {
+            switch (shape)
+            {
+                case ShapeType.Rectangle:
+                    return new Rectangle();
+                case ShapeType.Ellipse:
+                    return new Ellipse();
+            }
+            return new Ellipse();
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shape Parameter´s Calculator in C#\r");
+            Menu();
+            string inputValue = Console.ReadLine();
+
+            while (inputValue != "q")
+            {
+                switch (inputValue)
+                {
+                    case "1":
+                        var parametersRectangle = GetDataFromUser();
+     
+                        Shape rectangle = CreateShape(ShapeType.Rectangle);
+                        rectangle.Length = parametersRectangle[0];
+                        rectangle.Width = parametersRectangle[1];
+                        ViewShapeInfo(rectangle);
+                        inputValue = Console.ReadLine();
+                        break;
+                    case "2":
+                        var parametersEllipse = GetDataFromUser();
+                        Shape ellipse = CreateShape(ShapeType.Ellipse);
+                        ellipse.Length = parametersEllipse[0];
+                        ellipse.Width = parametersEllipse[1];
+                        ViewShapeInfo(ellipse);
+                        inputValue = Console.ReadLine();
+                        break;
+                    case "q":
+                        break;
+                    default:
+                        Console.WriteLine("This option is not defined. Try again!");
+                        inputValue = Console.ReadLine();
+                        break;
+                }
+            }
+        }
+
+
+        public static void ViewShapeInfo(Shape shape)
+        {
+            Console.WriteLine(shape.ToString());
+            Menu();
+            
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("------------------------\n");
+            Console.WriteLine("Choose one option and press Enter:");
+            Console.WriteLine("1 - I want to know info about rectangle");
+            Console.WriteLine("2 - I want to know info about ellipse");
+            Console.WriteLine("q - Exit");
+        }
+
+        public static List<double> GetDataFromUser()
+        {
+            string[] paramNames = { "length", "width" };
+            List<double> parameters = new List<double>();
+            foreach (string name in paramNames)
+            {
+                parameters.Add(CheckParameters(name));
+            }
+            return parameters;
+        }
+
+        public static double GetCheckedParameters(string name)
+        {
+            bool isSaved = false;
+            while (!isSaved)
+            {
+                Console.WriteLine("Insert " + name + " and press Enter: ");
+                string parameter = Console.ReadLine();
+                try
+                {
+                    double parameterFromUser = Convert.ToInt32(parameter);
+                    if (parameterFromUser > 0)
+                    {
+                        isSaved = true; 
+                        return parameterFromUser;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Parameter must be greater than 0");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Parameter must be numeric");
+                }
+            }
+            return 0;
+
+        }
+    }
+}
---
+++ Rektanglar och Ellipser/Shape.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rektanglar_och_Ellipser
+{
+    public abstract class Shape
+    {
+        private double _length;
+        private double _width;
+
+        public abstract double Area { get; }
+        public double Length 
+        {
+            get { return _length; }
+            set
+            {
+                if (value > 0)
+                {
+                    _length = value;
+                }
+                else
+                {
+                    throw new ApplicationException("Length must be a number greater than 0");
+                }
+            }
+        }
+        public abstract double Perimeter { get; }
+        public double Width 
+        {
+            get { return _width; }
+            set
+            {
+                if (value > 0)
+                {
+                    _width = value;
+                }
+                else
+                {
+                    throw new ApplicationException("Width must be a number greater than 0");
+                }
+            }
+        }
+
+        public Shape(double l, double w)
+        {
+            Length = l;
+            Width = w;
+        }
+
+        public Shape()
+        {
+        }
+
+        public override string ToString()
+        {
+            string result = 
+                "\n Längd: " + Length + 
+                "\n Bredd: " + Width + 
+                "\n Omkrets: " + Perimeter + 
+                "\n Area: " + Area;
+
+            return result;
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(double l, double w) : base()
+        {
+            Length = l;
+            Width = w;
+        }
+        
+        public override double Area
+        {
+            get => Length * Width;
+        }
+        public override double Perimeter
+        { 
+            get => 2 * (Length + Width); 
+        }
+    }
+
+    public class Ellipse : Shape
+    {
+        public Ellipse()
+        {
+        }
+
+        public Ellipse(double l, double w) : base()
+        {
+            Length = l;
+            Width = w;
+        }
+        public override double Area
+        {
+            get => 2 * Math.PI * Math.Sqrt((Length * Length + Width * Width) / 2);
+        }
+        public override double Perimeter
+        {
+            get => (Length / 2) * (Width / 2) * Math.PI; 
+        }
+    }
+}
